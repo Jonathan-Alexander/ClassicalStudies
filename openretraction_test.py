f35e653b868686db445f64543de7d9c42ee82615
Raghu1320 <raghu@vt.edu>
2019-02-28 13:35:41 -0500

    CSV parsing to get the DOIs

--- openretraction_test.py
+++ openretraction_test.py
@@ -1,6 +1,14 @@
 #Test script of integrating the open retraction database with PMC Data
 #Takes ID of article from articles list and prints JSON of it it's retracted
 import requests
+import csv
+
+def get_dois():
+    with open('file_name.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            # Instead of printing, make a GET request with each row["DOI"]
+            print(row["DOI"] + '\n')
 
 def is_retracted(doi):
     r = requests.get('http://openretractions.com/api/doi/{}/data.json'.format(doi))
