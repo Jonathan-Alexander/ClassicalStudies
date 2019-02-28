f74be0dfe51fb02ff854a7f8d35887041cb8efc9
Henry Wang <henrymw@vt.edu>
2019-02-28 13:39:54 -0500

    csv and open retraction api

--- openretraction_test.py
+++ openretraction_test.py
@@ -3,19 +3,25 @@
 import requests
 import csv
 
-def get_dois():
-    with open('file_name.csv', mode='r') as csv_file:
+def get_dois(filename):
+    with open(filename, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             # Instead of printing, make a GET request with each row["DOI"]
-            print(row["DOI"] + '\n')
+            # print(row["DOI"] + '\n')
+            print(is_retracted(row['DOI']))
 
 def is_retracted(doi):
-    r = requests.get('http://openretractions.com/api/doi/{}/data.json'.format(doi))
+    r = requests.get('http://openretractions.com/api/doi/{}/data.json'.format(doi)).json()
+    # print(r)
     if 'retracted' in r and r['retracted']:
         return True
     return False
 
 if __name__ == '__main__':
-    r = requests.get('http://openretractions.com/api/doi/10.1002/job.1787/data.json')
-    print(r.json())
+    # r = requests.get('http://openretractions.com/api/doi/10.1002/job.1787/data.json')
+
+    # print(is_retracted('10.1016/j.atherosclerosis.2009.08.003'))
+    # print(r.json())
+    
+    # top-level script stuff
