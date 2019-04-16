bb4f79f24381250d1ecad7e0d2665b9122d75117
jma <jma@vt.edu>
2019-04-16 09:36:47 -0400

    Added take_measurments file and updated full-sample-test.csv now contains titles

--- take_measurments.py
+++ take_measurments.py
@@ -16,14 +16,15 @@ JIF = []
 
 for index, row in sample.iterrows():
     
-    time.sleep(0.2)
+    time.sleep(0.33)
     issn = row["ISSN"] if (row["ISSN"] != "") else row["eISSN"]    
     pmid = row['PMID']
     pmcid = row['PMCID'][3:]
-    print(pmid)
-    web_xml_out = pp.parse_xml_web(pmid)
-    if(web_xml_out == None):
-        exit(0)
+    try:
+        web_xml_out = pp.parse_xml_web(pmid)
+    except Exception as e:
+        web_xml_out = {'authors':'Parse XML Web Failed for PMID: {}'.format(pmid), 'keywords':'Parse XML Web Failed for PMID: {}'.format(pmid)}
+        print('Parse XML Web Failed for PMID: {}'.format(pmid))
     num_citations = pp.parse_citation_web(pmcid, id_type ='PMC')
     times_cited = pp.parse_outgoing_citation_web(pmcid)
     if times_cited is not None:
@@ -47,13 +48,13 @@ for index, row in sample.iterrows():
     authors.append(num_authors)
     keys.append(keywords)
     JIF.append(impact_factor)
-    
+   
 sample['Citations'] = citations
 sample['Times Cited'] = t_cited
 sample['Number Authors'] = authors
 sample['Keywords'] = keys
 sample['Journal Impact Factor'] = JIF
-sample.to_csv('full-sample-test.csv', sep=',')
+sample.to_csv('full-sample-test.csv', sep=',', encoding='utf-8')
 
 
 
